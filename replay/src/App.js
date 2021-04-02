46858a6ddd786a7b7f896aa5b5dda6a73b8edf22
Shree Murthy <67122293+shmurthy024@users.noreply.github.com>
2021-04-01 18:09:52 -0700

    first-update!!!

--- replay/src/App.js
+++ replay/src/App.js
@@ -1,19 +1,46 @@
 import './App.css';
+import React from 'react';
+// Import Swiper React components
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/swiper.scss';
+import 'swiper/components/navigation/navigation.scss';
+import 'swiper/components/pagination/pagination.scss';
+import 'swiper/components/scrollbar/scrollbar.scss';
+
+
+
+// install Swiper modules
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="Header1">
-        <h1>
-          Welcome to Replay!
-        </h1>
-        <p>
-          Hello! Replay hopes to be a home for music lovers!
-        </p>
-        
-      </header>
+    
+    <div class="SwiperDiv">
+      <h1> Welcome to Replay </h1>
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log('slide change')}
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        ...
+      </Swiper>
+      
     </div>
   );
-}
+};
+
 
 export default App;
